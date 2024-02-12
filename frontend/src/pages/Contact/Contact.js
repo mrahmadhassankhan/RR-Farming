309b0227a2f40e90a876d55ac90fa28ae7584515
abubakarnangrii <abubakarnangri@gmail.com>
2024-02-13 00:17:52 +0500

    contact page upftedd

--- frontend/src/pages/Contact/Contact.js
+++ frontend/src/pages/Contact/Contact.js
@@ -36,29 +36,38 @@ const Contact = () => {
           <h1 className={CSS['contact-title']}>Contact Us</h1>
           <div className={CSS['contactus-container']}>
             <div className={CSS['contactus-img']}>
-              <img src={img} alt='img' width={'100px'} height={'100px'} />
+              <img className={CSS['img']} src={img} alt='img' />
             </div>
             <div className={CSS['contactus-details']}>
               <form onSubmit={handleFromEmail}>
                 <div className={CSS['contactus-name_email-container']}>
+
                   <div className={CSS['contactus-name-container']}>
-                    <label htmlFor="_fullName">Full Name<span>*</span></label>
+                    <label className={CSS['contactus-label']} htmlFor="_fullName">Full Name<span className={CSS['contactus-star']}>*</span></label>
                     <input className={CSS['contactus-name']} type="text" id="_fullName" name="_fullName" onChange={handleName}  value={name} placeholder={'Full Name'} required />
                   </div>
+
                   <div className={CSS['contactus-name-container']}>
-                    <label htmlFor="_email">Email Address<span>*</span></label>
+                    <label className={CSS['contactus-label']} htmlFor="_email">Email Address<span className={CSS['contactus-star']}>*</span></label>
                     <input className={CSS['contactus-email']} type="text" id="_email" name="_email" onChange={handleEmail} value={email} placeholder={'Email Address'} required />
                   </div>
+
                 </div>
-                <div>
-                  <label htmlFor="_subject">Subject<span>*</span></label>
+
+
+                <div className={CSS['contactus-subject-container']}>
+                  <label className={CSS['contactus-label']} htmlFor="_subject">Subject<span className={CSS['contactus-star']}>*</span></label>
                   <input className={CSS['contactus-subject']} type="text" id="_subject" name="_subject" onChange={handleSubject} value={subject} placeholder={'Subject'} required />
                 </div>
-                <div>
-                  <label htmlFor="_message">Message<span>*</span></label>
+
+
+                <div className={CSS['contactus-message-container']}>
+                  <label className={CSS['contactus-label']} htmlFor="_message">Message<span className={CSS['contactus-star']}>*</span></label>
                   <textarea rows={3} className={CSS['contactus-message']} id="_message" name="_message" onChange={handleMessage} value={message} placeholder={'Message'} required ></textarea>
                 </div>
-                <button type='button'>Send Message</button>
+
+
+                <button className={CSS['send-btn']} type='button'>Send Message</button>
               </form>
             </div>
           </div>
