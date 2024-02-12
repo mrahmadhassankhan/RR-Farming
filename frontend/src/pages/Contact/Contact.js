8240ce1822479e65ebc396075efb2af82b0b9924
abubakarnangrii <abubakarnangri@gmail.com>
2024-02-12 22:25:46 +0500

    Add to Cart done

--- frontend/src/pages/Contact/Contact.js
+++ frontend/src/pages/Contact/Contact.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
-import CSS from './Contact.module.css'
-import img from '../../images/contact.png'
+import React, { useState } from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import CSS from './Contact.module.css';
+import img from '../../images/contact.png';
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubject = (e) => {
+    setSubject(e.target.value);
+  };
+
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted with:', name, email, subject, message);
+    // You can add your logic to send the data to the server or perform any other actions.
+  };
+
   return (
     <div>
       <Header />
@@ -13,27 +40,60 @@ const Contact = () => {
           <h1 className={CSS['contact-title']}>Contact Us</h1>
           <div className={CSS['contactus-container']}>
             <div className={CSS['contactus-img']}>
-              <img src={img} alt='img' width={'550px'} height={'550px'} />
+              <img src={img} alt='img' width={'100px'} height={'100px'} />
             </div>
             <div className={CSS['contactus-details']}>
-              <form onSubmit={"gjhgjhg"}>
-                <div>
-                  <div className={CSS['contactus-details']}>
-                    <label htmlFor="fullName">Full Name <span>*</span></label>
-                    <input type="text" id="fullName" name="fullName" />
+              <form onSubmit={handleFormSubmit}>
+                <div className={CSS['contactus-name_email-container']}>
+                  <div className={CSS['contactus-name-container']}>
+                    <label htmlFor="_fullName">Full Name<span>*</span></label>
+                    <input
+                      className={CSS['contactus-name']}
+                      type="text"
+                      id="_fullName"
+                      name="_fullName"
+                      onChange={handleName}
+                      value={name}
+                      placeholder={'Full Name'}
+                      required
+                    />
                   </div>
-                  <div>
-                    <label htmlFor="fullName">Email Address <span>*</span></label>
-                    <input type="text" id="fullName" name="fullName" />
+                  <div className={CSS['contactus-name-container']}>
+                    <label htmlFor="_email">Email Address<span>*</span></label>
+                    <input
+                      className={CSS['contactus-email']}
+                      type="text"
+                      id="_email"
+                      name="_email"
+                      onChange={handleEmail}
+                      value={email}
+                      placeholder={'Email Address'}
+                      required
+                    />
                   </div>
                 </div>
                 <div>
-                  <label htmlFor="fullName">Subject <span>*</span></label>
-                  <input type="text" id="fullName" name="fullName" />
+                  <label htmlFor="_subject">Subject<span>*</span></label>
+                  <input
+                    type="text"
+                    id="_subject"
+                    name="_subject"
+                    onChange={handleSubject}
+                    value={subject}
+                    placeholder={'Subject'}
+                    required
+                  />
                 </div>
                 <div>
-                  <label htmlFor="message">Message <span>*</span></label>
-                  <textarea id="message" name="message"></textarea>
+                  <label htmlFor="_message">Message<span>*</span></label>
+                  <textarea
+                    id="_message"
+                    name="_message"
+                    onChange={handleMessage}
+                    value={message}
+                    placeholder={'Message'}
+                    required
+                  ></textarea>
                 </div>
                 <button type='submit'>Send Message</button>
               </form>
@@ -43,7 +103,7 @@ const Contact = () => {
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
